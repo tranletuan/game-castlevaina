4c79fcccdf27c0c6e662314edb7310ac02536cd4
tranletuanuit@gmail.com <tranletuanuit@gmail.com@4f3f3cca-21bd-a453-d0ef-e37d6f0e6bc5>
2014-11-06 05:16:25 +0000

    TuanTL : bill detect collision

--- NMLTG/NMLTG/world_test.cpp
+++ NMLTG/NMLTG/world_test.cpp
@@ -8,8 +8,7 @@ WorldTest::WorldTest(int cmd_show) : CGame(cmd_show)
 	this->bill = new CBill(123, Player1, D3DXVECTOR3(100, 190, 0));
 	this->bill2 = new CBill(23, Player1, D3DXVECTOR3(50, 190, 0));
 	this->map_reader = new CMapReader(L"map1");
-	bill->_physical.vx = BILL_VX;
-	bill2->_physical.vx = BILL_VX;
+	bill->_physical.vx_last = -1;
 
 	test = 0;
 }
@@ -36,6 +35,7 @@ void WorldTest::RenderFrame(LPDIRECT3DDEVICE9 d3d_device)
 	}
 
 	bill->Draw();
+	waepon->Draw();
 
 	string show = to_string(bill->_physical.current_vy) + " : " + to_string(bill->_physical.current_vx) + 
 		" : " + to_string(test) + " : " + to_string(bill->_physical.y);
@@ -57,9 +57,11 @@ void WorldTest::ProcessInput(LPDIRECT3DDEVICE9 d3d_device, int delta)
 	//Set phím hướng súng
 	if (IsKeyDown(DIK_W))
 	{
+		bill->SetGunDirection(Up);
 	}
 	else if (IsKeyDown(DIK_S))
 	{
+		bill->SetGunDirection(Down);
 	}
 
 } //Xử lý phím
@@ -70,6 +72,7 @@ void WorldTest::OnKeyDown(int key_code)
 	{
 	case DIK_1:
 		waepon->SetWaeponType(NBullet);
+		bill->Dying();
 		break;
 	case DIK_2:
 		waepon->SetWaeponType(MBullet);
@@ -84,9 +87,10 @@ void WorldTest::OnKeyDown(int key_code)
 		waepon->SetWaeponType(SBullet);
 		break;
 	case DIK_J:
-		bill->SetStatus(Jump);
-		bill->_physical.vy = 8.f;
-		
+		bill->Jumping();
+		break;
+	case DIK_L:
+		bill->Attacking(waepon);
 		break;
 	}
 }
@@ -97,11 +101,12 @@ void WorldTest::OnKeyUp(int key_code)
 	{
 	case DIK_D:
 	case DIK_A:
-		bill->Standing();
+		bill->Moving(0);
 		break;
 
 	case DIK_W:
 	case DIK_S:
+		bill->SetGunDirection(Normal);
 		break;
 	case DIK_J:
 		break;
@@ -118,16 +123,10 @@ void WorldTest::GameUpdate(int delta_time)
 	{
 		CObject* ground = (*i).second;
 		cd = bill->_physical.Collision(&ground->_physical);
-		
-		//if (cd == TopCollision)
-			test = cd;
 
 		if (cd == TopCollision)
 		{
-			bill->_physical.n = GRAVITY;
-			bill->_physical.y = ground->_physical.y + 33;
-			bill->_physical.time_in_space = 0;
-			 
+			bill->Standing(ground->_physical.y + 33);
 			break;
 		}
 	}
@@ -136,22 +135,10 @@ void WorldTest::GameUpdate(int delta_time)
 	if (cd != TopCollision)
 	{
 		bill->_physical.n = 0;
-		bill->_physical.ground = 0;
 	}
 
 	bill->Update(delta_time);
-
-	if (bill->_physical.x < 0)
-	{
-		bill->_physical.x = 0;
-		bill->_physical.vx = BILL_VX;
-	}
-	
-	if (bill->_physical.x > kScreenWidth)
-	{
-		bill->_physical.vx = -BILL_VX;
-	}
-
+	waepon->Update(delta_time);
 	//if (bill->_physical.vx > 0 )
-	//camera->UpdateCamera(bill->_physical.x);
+	camera->UpdateCamera(bill->_physical.x);
 }
