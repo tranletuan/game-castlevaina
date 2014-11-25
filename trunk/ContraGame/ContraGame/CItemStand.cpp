43a0edfa967519818920a254d6677979f86e70a4
ttienqb@gmail.com <ttienqb@gmail.com@4f3f3cca-21bd-a453-d0ef-e37d6f0e6bc5>
2014-11-25 08:03:45 +0000

    Update Object Item Stand

--- trunk/ContraGame/ContraGame/CItemStand.cpp
+++ trunk/ContraGame/ContraGame/CItemStand.cpp
@@ -5,6 +5,7 @@ CItemStand::CItemStand(int id, SpecificType specific_type, D3DXVECTOR3 pos, int 
 {
 	_hp = 1;
 	LoadResources();
+	_state_item_stand = SIS_Close;
 }
 
 void CItemStand::LoadResources()
@@ -14,72 +15,123 @@ void CItemStand::LoadResources()
 	_sprite_item = new CSprite(rs->_item);
 	_sprite_stand = new CSprite(rs->_item_stand);
 
+	// lấy index của _sprite_item cho mỗi loại đạn
 	switch (_specific_type)
 	{
-	case ItemM_Stand:
-		_pos_stand = D3DXVECTOR3(_physical.x, _physical.y, 0);
+	case ItemM_Stand:		
 		_sprite_item->SelectFrameOf(1);
 		break;
 	default:
 		break;
 	}
-	CCamera *_cam = CResourcesManager::GetInstance()->_camera;
-	_pos_stand = _cam->Transform(_pos_stand.x, _pos_stand.y);
-
 }
 
 void CItemStand::Draw()
 {
 	CCamera *_cam = CResourcesManager::GetInstance()->_camera;
-	D3DXVECTOR3 pos = _cam->Transform(_physical.x, _physical.y);
-	// khi posX + width/2 < posX + width của camera cho trương hợp hướng camera di chuyển ngang   
-	if (_physical.x + _sprite_stand->sprite_texture->frame_width / 2 >= _cam->getPosX() + _cam->getWidth()
-		&& _physical.y + _sprite_stand->sprite_texture->frame_height / 2 >= _cam->getPosY())
+	D3DXVECTOR3 pos;
+	if (_hp>0)
 	{
-		DrawWhenStand();
+		_pos_stand = _cam->Transform(_physical.x, _physical.y);
 	}
 	else
 	{
-		if (_hp > 0)
-		{
-			DrawWhenActivity();
-		}
+		pos = _cam->Transform(_physical.x, _physical.y);
 	}
-	if (_hp == 0)
+	// Xử lý vẽ theo state item
+	switch (_state_item_stand)
 	{
+	case SIS_Close:
+		DrawWhenStand(_pos_stand);
+		break;
+	case SIS_Open:
+	case SIS_Enable:
+		DrawWhenActivity(_pos_stand);
+		break;
+	case SIS_Spatter:
 		DrawWhenAttack(pos);
+		break;
+	default:
+		break;
 	}
 }
 
 void CItemStand::Update(int delta_time)
 {
-	if (_hp == 0 && _physical.current_vy >= 0)
+	CCamera *_cam = CResourcesManager::GetInstance()->_camera;
+	int levelMap = CResourcesManager::GetInstance()->m_levelMap;
+	// chuyển state item
+	// Xét khoảng hoạt động của item stand the o level map
+	switch (levelMap)
 	{
-		_physical.vx = 0.01f;
-		_physical.vy = ENEMY_VY_DIE;
+	case 1:
+	case 3:
+		if (_physical.x + _sprite_stand->sprite_texture->frame_width / 2 < _cam->getPosX() + _cam->getWidth() && 
+			_physical.x - _sprite_stand->sprite_texture->frame_width / 2 > _cam->getPosX())
+		{
+			_state_item_stand = SIS_Open;
+		}
+		else
+		{
+			_state_item_stand = SIS_Close;
+		}
+		break;
+	case 2:
+		if (_physical.y + _sprite_stand->sprite_texture->frame_height / 2 < _cam->getPosY()
+			&& _physical.y - _sprite_stand->sprite_texture->frame_height / 2 > 0)
+		{
+			_state_item_stand = SIS_Open;
+		}
+		else
+		{
+			_state_item_stand = SIS_Close;
+		}
+		break;
+	default:
+		break;
+	}
+
+	if (_state_item_stand == SIS_Open)
+	{
+		if (_sprite_stand->index > 3 && _hp > 0)
+		{
+			_state_item_stand = SIS_Enable;
+		}
+	}
+	
+
+	// set vx & vy item văng lên
+	if (_hp == 0 )
+	{
+		_state_item_stand = SIS_Spatter;
+		_physical.vx = ITEM_VX_ENABLE;
+		_physical.vy = ITEM_VY_ENABLE;
 		_physical.CalcPositionWithGravitation(delta_time, GRAVITY);
 	}
 }
 
-void CItemStand::DrawWhenStand()
+void CItemStand::DrawWhenStand(D3DXVECTOR3 pos)
 {
 	// Khi chưa hoạt động chỉ ở index = 0;	
-	_sprite_stand->DrawWithDirectionAndOneTimeEffect(_pos_stand, _physical.vx_last, 0, 0);
+	_sprite_stand->SelectFrameOf(0);
+	_sprite_stand->Draw(pos.x, pos.y);
 }
 
-void CItemStand::DrawWhenActivity()
+void CItemStand::DrawWhenActivity(D3DXVECTOR3 pos)
 {
 	// Khi hoạt động gán vô 1 animation cho nó
-	_sprite_stand->DrawWithDirectionAndOneTimeEffect(_pos_stand, _physical.vx_last, 0, 7);
+	_sprite_stand->DrawWithDirection(_pos_stand, _physical.vx_last, 0, 6,200);
 }
 
 void CItemStand::DrawWhenAttack(D3DXVECTOR3 pos)
-{
+{	
 	// Khi bị bắt không vẽ _sprite_stand, _sprite_effect nổ theo _pos_stand , đồng thời vẻ _sprite_item vang lên
-	_sprite_effect->DrawWithDirection(_pos_stand, _physical.vx_last, 0, 2);
-	D3DXVECTOR2 pos2 = D3DXVECTOR2(pos.x, pos.y);
-	_sprite_item->Draw(pos2);
-
+	if (_sprite_effect->index != 2 )
+	{
+		 _sprite_effect->DrawWithDirectionAndOneTimeEffect(_pos_stand, _physical.vx_last, 0, 2,200);
+	}
+	// Vẽ item văng lên	
+	_sprite_item->Draw(pos.x, pos.y);
 }
 
 CItemStand::~CItemStand()
