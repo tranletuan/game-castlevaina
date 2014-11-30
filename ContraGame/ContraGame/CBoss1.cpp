b2a071eace2728bd23dcfd9121babd4ac379f17f
ttienqb@gmail.com <ttienqb@gmail.com@4f3f3cca-21bd-a453-d0ef-e37d6f0e6bc5>
2014-11-30 15:35:33 +0000

    Update: CBoss Map 1

--- ContraGame/ContraGame/CBoss1.cpp
+++ ContraGame/ContraGame/CBoss1.cpp
@@ -133,8 +133,8 @@ void CBoss1::DrawWhenBadgeDie(D3DXVECTOR3 pos)
 void CBoss1::DrawWhenDestroyBG1(D3DXVECTOR3 pos)
 {
 	_sprite_bg->DrawWithDirectionAndOneTimeEffect(pos, 1, 0, 1, 100);
-	D3DXVECTOR3 pos_draw = D3DXVECTOR3(pos.x - BOSS1_SPACE_DESTROY_X, pos.y + 30, 0);
-	D3DXVECTOR3 pos_draw1 = D3DXVECTOR3(pos.x - BOSS1_SPACE_DESTROY_X, pos.y + 30 + BOSS1_SPACE_DESTROY_Y, 0);
+	D3DXVECTOR3 pos_draw = D3DXVECTOR3(pos.x - 10 - BOSS1_SPACE_DESTROY_X, pos.y + 30, 0);
+	D3DXVECTOR3 pos_draw1 = D3DXVECTOR3(pos.x - 10 - BOSS1_SPACE_DESTROY_X, pos.y + 30 + BOSS1_SPACE_DESTROY_Y, 0);
 	_sprite_effect1->DrawWithDirectionAndOneTimeEffect(pos_draw, 1, 0, 2, 300);
 	_sprite_effect1->DrawWithDirectionAndOneTimeEffect(pos_draw1, 1, 0, 2, 300);
 }
