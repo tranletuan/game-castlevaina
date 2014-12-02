981d79ae7fa51f925b3f9b6610c90efcf2e3d503
ttienqb@gmail.com <ttienqb@gmail.com@4f3f3cca-21bd-a453-d0ef-e37d6f0e6bc5>
2014-12-02 08:56:41 +0000

    Update: CGroundEffect

---
+++ trunk/ContraGame/ContraGame/CGroundEffect.cpp
@@ -0,0 +1,71 @@
+﻿#include "CGroundEffect.h"
+
+CGroundEffect::CGroundEffect(int id, SpecificType specific_type, D3DXVECTOR3 pos, int width, int height)
+	:CGround(id, specific_type, pos, width, height)
+{
+	_count = width / GROUND_SIZE_NORMAL;
+	_time_count = 0;
+	_isDraw = false;
+	LoadResources();
+}
+
+CGroundEffect::~CGroundEffect()
+{
+	if (_current_sprite != NULL)
+	{
+		delete _current_sprite;
+	}
+}
+
+void CGroundEffect::LoadResources()
+{
+	CResourcesManager* rs = CResourcesManager::GetInstance();
+	switch (_specific_type)
+	{
+	
+		break;
+	case Ground_Effect:
+		_current_sprite = new CSprite(rs->_ground_effect1);
+		break;
+	}
+}
+
+void CGroundEffect::Update(int delta_time)
+{
+
+	switch (_specific_type)
+	{
+	case Ground_Effect:
+		_time_count++;
+		_isDraw = false;
+		if (_time_count > 10000)
+		{
+			_time_count = 0;
+		}
+		if (_time_count % 10 > 4 && _time_count % 10 < 8)
+		{
+			_isDraw = true;
+		}
+
+		break;
+	}
+}
+
+void CGroundEffect::Draw()
+{
+	if (_isDraw)
+	{
+		//Chỉ vẽ để kiểm tra, khi chạy game đoạn code sẽ bị xóa
+		int x = _physical.bounds.left + GROUND_SIZE_NORMAL / 2;
+
+		CCamera* c = CResourcesManager::GetInstance()->_camera;
+		D3DXVECTOR3 pos = c->Transform(x, _physical.y);
+
+		for (int i = 0; i < _count; i++)
+		{
+			_current_sprite->Draw(pos.x, pos.y);
+			pos.x += GROUND_SIZE_NORMAL;
+		}
+	}
+
+}
