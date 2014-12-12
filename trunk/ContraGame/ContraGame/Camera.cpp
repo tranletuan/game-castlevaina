dd6c0d94d9a569fdca45819cecb8b4bf9c1d8b0d
ttienqb@gmail.com <ttienqb@gmail.com@4f3f3cca-21bd-a453-d0ef-e37d6f0e6bc5>
2014-12-12 04:55:37 +0000

    Update: CAMERA

--- trunk/ContraGame/ContraGame/Camera.cpp
+++ trunk/ContraGame/ContraGame/Camera.cpp
@@ -4,18 +4,24 @@ CCamera::CCamera()
 {
 	view_port.x = 0;
 	view_port.y = kScreenHeight;
-	
+	_cam_state == CamS_Follow;
+	_vX = 2;
+
 }
 
 CCamera::CCamera(float posX, float posY)
 {
 	view_port.x = posX;
 	view_port.y = posY;
+	_cam_state == CamS_Follow;
+	_isMove = false;
+	_isStop = false;
+	_vX = 0.08;
 }
 
 void CCamera::UpdateCameraX(float x)
 {
-	
+
 	view_port.x = (int)x - kScreenWidth / 2;
 	if (view_port.x < 0)
 	{
@@ -32,19 +38,43 @@ void CCamera::UpdateCameraY(float y)
 	}
 }
 
-void CCamera::Update()
+void CCamera::Update(int time)
 {
 	CResourcesManager* rs = CResourcesManager::GetInstance();
 	switch (rs->m_levelMap)
 	{
 	case 1:
-		UpdateCameraX(rs->m_posBill.x);
+		if (!_isMove)
+		{
+			if (view_port.x >= 0 && view_port.x <= rs->m_widthMap - kScreenWidth)
+			{
+				if ((int)rs->m_posBill.x < rs->m_widthMap - kScreenWidth)
+				{
+					if (!_isStop)
+					{
+						UpdateCameraX(rs->m_posBill.x);
+					}
+				}
+				else
+				{
+					_isMove = true;
+				}
+			}
+		}
+		else
+		{
+			MoveDistanceX(128, time);
+		}
 		break;
 	case 2:
-		UpdateCameraY(rs->m_posBill.y);
+		if (view_port.y >= 0 && view_port.x <= rs->m_heightMap)
+		{
+			UpdateCameraY(rs->m_posBill.y);
+		}
 		break;
 	}
 	rs->_camera = this;
+
 }
 
 //Hàm để chuyển tọa độ góc của đối tượng thành tọa đô tậm
@@ -94,4 +124,18 @@ void CCamera::processInput()
 D3DXVECTOR3 CCamera::Transform(D3DXVECTOR3 pos)
 {
 	return Transform(pos.x, pos.y);
+}
+
+void CCamera::MoveDistanceX(float distance, int time)
+{
+	if (view_port.x < CResourcesManager::GetInstance()->m_widthMap - kScreenWidth)
+	{
+		int delta = (int)time*_vX;
+		view_port.x += delta;
+	}
+	else
+	{
+		_isStop = true;
+	}
+
 }
