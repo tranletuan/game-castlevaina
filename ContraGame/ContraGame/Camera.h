c2a3a84445310cd5114050dc249344f8b57c1ecd
ttienqb@gmail.com <ttienqb@gmail.com@4f3f3cca-21bd-a453-d0ef-e37d6f0e6bc5>
2014-12-12 04:55:37 +0000

    Update: CAMERA

--- ContraGame/ContraGame/Camera.h
+++ ContraGame/ContraGame/Camera.h
@@ -1,4 +1,4 @@
-#ifndef CAMERA_H
+﻿#ifndef CAMERA_H
 #define CAMERA_H
 
 #include <d3d9.h>
@@ -6,9 +6,20 @@
 #include "Global.h"
 #include "CResourcesManager.h"
 
+enum CamState
+{
+	CamS_Move, // tự đông di chuyển
+	CamS_Follow, // theo con bill
+	CamS_Stop // dừng lại
+};
 class CResourcesManager;
 class CCamera
 {
+private:
+	CamState _cam_state;
+	bool _isMove;
+	bool _isStop;
+	float _vX; // Vận tốc vX
 
 public:
 	D3DXVECTOR2 view_port;
@@ -17,7 +28,7 @@ public:
 
 	void UpdateCameraX(float x);
 	void UpdateCameraY(float y);
-	void Update();
+	void Update(int time);
 	D3DXVECTOR3 CenterSprite(int x, int y, int width, int height);
 	D3DXVECTOR3 Transform(float x, float y);
 	D3DXVECTOR3 Transform(D3DXVECTOR3 pos);
@@ -27,7 +38,13 @@ public:
 	float getPosX(){ return view_port.x; }
 	float getPosY(){ return view_port.y; }
 	int getWidth(){ return kScreenWidth; }
-	int getHeight(){ return kScreenHeight; }
+	int getHeight(){ return kScreenHeight; }	
+	void setVx(float x){ _vX = 0; }
+	float getVx(){ return _vX; }
+	void setState(CamState x){ _cam_state = x; }
+	CamState getState(){ return _cam_state; }
+
+	void MoveDistanceX( float distance, int time); // Di chuyển 1 khoảng cách khi tới điểm theo X
 };
 
 
